e0cbcedd1e9c3153abde3a753ba8fd6371de925a
Korotaev Danil <greyd@mail333.com>
2012-05-18 02:14:00 +0600

    Added test for FlightController#search without implementation

--- spec/controllers/flights_controller_spec.rb
+++ spec/controllers/flights_controller_spec.rb
@@ -6,12 +6,15 @@ describe FlightsController do
     @correct_attributes = {
       :price => 1.00,
       :hops_attributes => [{
-        :latstart => 11.111111,
-        :lonstart => 111.111111,
-        :latend => 11.111111,
-        :lonend => 111.111111,
+        :start => [11.111111,111.111111],
+        :end => [22.111111,122.222222],
         :date => Time.now,
-        :duration => 1
+        :duration => 1.hour
+      },{
+        :start => [22.222222,122.222222],
+        :end => [33.333333,133.333333],
+        :date => Time.now,
+        :duration => 2.hours
       }]
     }
   end
@@ -20,9 +23,29 @@ describe FlightsController do
     it { subject; assigns(:flight).should be_an_instance_of(Flight) }
     it { expect {subject}.to change(Flight, :count).by(1) }
     it { subject; response.status.should be == 201 }
-    describe "с некорректными данными" do
+    describe "с некорректными атрибутами" do
       subject { post :create, :flight => {}, :format => :json }
       it { subject; response.status.should be == 422 }
     end
+  end
+  describe "GET :search" do
+    before do
+      @flights = [
+        Flight.create!(@correct_attributes),
+        Flight.create!(@correct_attributes.merge(:price => 2.00))
+      ]
+      @search_attributes = {
+        :start => @correct_attributes[:hops_attributes][0][:start],
+        :end => @correct_attributes[:hops_attributes][1][:end],
+        :max_price => 1.00,
+        :from_date => Time.now,
+        :to_date => Time.now + 1.day,
+        :max_transfers_count => 1,
+        :max_way_duration => 1.hour
+      }
+    end
+    subject { get :search, @search_attributes }
+    it { subject; assigns(:found_ways).each {|found_way| found_way.should be_an_instance_of(WaySearch) } }
+    it { subject; assigns(:found_ways).each {|found_way| found_way.flights.should be == [@flight] } }
   end
 end
